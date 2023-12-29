35a3ee25b763873b10de77c7d75031b7eaf005c9
Aayush <aayush.paigwar123@gmail.com>
2023-12-29 14:41:42 +0530

    Working implementation for Auth using Supabase in flutter🔥

---
+++ lib/homepage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text('Home Page'),
+          ),
+          Gap(10),
+          Center(child: Text('Signed In Successfully!'))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_supabase_auth/signin.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(anonKey: 'SUPABSE ANON KEY', url: 'SUPABASE URL');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Flutter X Supabase Auth')),
+        body: const SignIn(),
+      ),
+    );
+  }
+}
