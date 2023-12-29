9eef59f335efad50ed6ff6f83365d54f7993e703
Aayush <aayush.paigwar123@gmail.com>
2023-12-29 15:51:34 +0530

    Delete Unwanted Files

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(anonKey: 'SUPABSE ANON KEY', url: 'SUPABASE URL');
+  await Supabase.initialize(
+      anonKey:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdhYW11aHpmcGZkdmZha2RmemV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NjgxOTEsImV4cCI6MjAxNzU0NDE5MX0.3oRlKrnP3oRs0uck4G5rgDkebdkehGVmd_YG_pSprvI',
+      url: 'https://gaamuhzfpfdvfakdfzew.supabase.co URL');
   runApp(const MyApp());
 }
 
